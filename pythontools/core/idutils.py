e874c50a3536b9a8259d0e47d8331f2cafe87b96
pancx <p_lord@163.com>
2024-07-31 14:58:26 +0800

    🦄 refactor(core): 重构工具类函数注释
    1. 更改IDCardUtil方法函数注释
    2. 更改sex -> gender，统一性别表达；
    3. 更改性别码生成逻辑，用户不用再传递特定的枚举实例，而是传递字符串即可。

--- pythontools/core/idutils.py
+++ pythontools/core/idutils.py
@@ -42,39 +42,55 @@ class IDCard(object):
             f"area='{self.get_area()}', "
             f"birthday='{self.birthday.strftime('%Y-%m-%d')}', "
             f"age={self.get_age()}, "
-            f"sex={self.get_sex()}"
+            f"gender={self.get_gender()}"
             f")"
         )
 
     def get_age(self) -> float:
         """
         获取年龄
-        :return:
+
+        Returns
+        -------
+        float
+            年龄
         """
         # NOTE 应该有更好的计算方式
         return DatetimeUtil.get_age(self.birthday)
 
-    def get_sex(self) -> str:
+    def get_gender(self) -> str:
         """
         获取性别
-        :return: 男性或女性
+
+        Returns
+        -------
+        str
+            性别
         """
-        sex_code = StringUtil.sub_lst(self.id, 16, 17)
-        sex_code = BasicConvertor.to_int(sex_code)
-        sex_obj = Gender.get_sex(sex_code)
-        return sex_obj.value.sex
+        gender_code = StringUtil.sub_lst(self.id, 16, 17)
+        gender_code = BasicConvertor.to_int(gender_code)
+        gender_obj = Gender.get_gender_by_code(gender_code)
+        return gender_obj.value.sex
 
     def get_province(self) -> str:
         """
-        获取省份名称
-        :return: 省份名称
+        获取登记省份信息
+
+        Returns
+        -------
+        str
+            省份信息
         """
         return PRO_DICT.get(BasicConvertor.to_str(self.province_code))
 
     def get_area(self) -> str:
         """
-        获取地区名称
-        :return: 地区名称
+        获取地区信息
+
+        Returns
+        -------
+        str
+            地区信息
         """
         return AREA_INFO.get(self.area_code)
 
@@ -87,68 +103,118 @@ class IDCardUtil(object):
     AREA_LST: typing.List[str] = list(AREA_INFO.keys())
 
     @classmethod
-    def generate_random_valid_card(cls, *, sex=Gender.MALE) -> IDCard:
+    def generate_random_valid_card(cls, gender: str = "男") -> IDCard:
         """
-        随机产生 IDCard 实例, 默认使用18位身份证
-        :param sex: 性别
-        :return: IDCard 对象
+        构造随机身份证实例
+
+        Parameters
+        ----------
+        gender : str, optional
+            性别, by default 男
+
+        Returns
+        -------
+        IDCard
+            身份证实例
         """
         default_code_length = 18
+        gender = Gender.get_gender_by_name(gender)
         random_id = cls.generate_random_valid_id(
-            code_length=default_code_length, sex=sex
+            code_length=default_code_length, gender=gender
         )
         return cls.get_card_from_id(random_id)
 
     @classmethod
-    def generate_random_valid_id(cls, *, code_length: int = 18, sex=Gender.MALE) -> str:
+    def generate_random_valid_id(
+        cls, *, code_length: int = 18, gender=Gender.MALE
+    ) -> str:
         """
-        获取随机的身份证 ID
-        :exception: ValueError
-        :param code_length: 身份证长度
-        :param sex: 性别
-        :return: 随机的身份证ID
+        获取随机ID
+
+        Parameters
+        ----------
+        code_length : int, optional
+            身份证ID长度, by default 18
+        gender : Gender, optional
+            指定性别, by default Gender.MALE
+
+        Returns
+        -------
+        str
+            随机ID
+
+        Raises
+        ------
+        ValueError
+            如果指定的ID长度不是15或者18, 则抛出异常
         """
         if code_length == cls.CHINA_ID_MAX_LENGTH:
-            return cls.generate_random_valid_18_id(sex=sex)
+            return cls.generate_random_valid_18_id(gender=gender)
 
         if code_length == cls.CHINA_ID_MIN_LENGTH:
-            return cls.generate_random_valid_15_id(sex=sex)
+            return cls.generate_random_valid_15_id(gender=gender)
         else:
             raise ValueError("code_length must be 15 or 18")
 
     @classmethod
-    def generate_random_valid_18_id(cls, *, sex=Gender.MALE) -> str:
+    def generate_random_valid_18_id(cls, *, gender: str = "男") -> str:
         """
-        生成18位身份证
-        :param sex: 性别
-        :return: 18位身份证
+        生成 18 位随机ID
+
+        Parameters
+        ----------
+        gender : str, optional
+            性别, by default Gender.MALE
+
+        Returns
+        -------
+        str
+            18 位随机ID
         """
         area = random.choice(cls.AREA_LST)
         birthday = DatetimeUtil.get_random_date()
         birthday_format_str = birthday.strftime("%Y%m%d")
         sequence_code = random.randint(10, 99)
-        sex_code = sex.value.sex_code
-        code17 = f"{area}{birthday_format_str}{sequence_code}{sex_code}"
+        gender_enum_obj = Gender.get_gender_by_name(gender)
+        gender_code = gender_enum_obj.value.sex_code
+        code17 = f"{area}{birthday_format_str}{sequence_code}{gender_code}"
         checksum_code = cls.get_check_sum(code17)
         code18 = f"{code17}{checksum_code}"
 
         return code18
 
     @classmethod
-    def generate_random_valid_15_id(cls, *, sex=Gender.MALE) -> str:
+    def generate_random_valid_15_id(cls, *, gender: str = "男") -> str:
         """
         生成15位身份证
-        :param sex: 性别
-        :return: 15位身份证
+
+        Parameters
+        ----------
+        gender : str, optional
+            性别, by default "男"
+
+        Returns
+        -------
+        str
+            随机15位身份证
         """
+        # TODO 实现15位身份证生成逻辑
         raise NotImplementedError()
 
     @classmethod
     def is_valid_id(cls, s: str) -> bool:
         """
         判断是否是合规的身份证 ID
-        :param s: 待检测 ID
-        :return: 如果合规返回 True, 否则返回 False
+
+        Parameters
+        ----------
+        s : str
+            待检测身份证 ID
+
+        Returns
+        -------
+        bool
+            是否是合规 ID
         """
         if not StringUtil.is_string(s):
             return False
@@ -164,8 +230,16 @@ class IDCardUtil(object):
     def is_valid_id_18(cls, s: str) -> bool:
         """
         是否是合规的18位身份证
-        :param s: 待检测身份证 ID
-        :return: 如果合规返回 True, 否则返回 False
+
+        Parameters
+        ----------
+        s : str
+            待检测18位身份证
+
+        Returns
+        -------
+        bool
+            是否是合规18位身份证
         """
         if cls.CHINA_ID_MAX_LENGTH != len(s):
             return False
@@ -187,17 +261,35 @@ class IDCardUtil(object):
     def is_valid_id_15(cls, s: str) -> bool:
         """
         检查是否是合规的15位身份证
-        :param s: 待检测身份证 ID
-        :return: 如果合规返回 True, 否则返回 False
+
+        Parameters
+        ----------
+        s : str
+            待检测15位身份证
+
+        Returns
+        -------
+        bool
+            是否合规
+
         """
+        # TODO 实现15位身份证校验逻辑
         raise NotImplementedError()
 
     @classmethod
     def get_check_sum(cls, s: str) -> str:
         """
         根据身份证计算最后的校验位
-        :param s: 身份证 ID
-        :return: 校验字符
+
+        Parameters
+        ----------
+        s : str
+            身份证ID前n-1位
+
+        Returns
+        -------
+        str
+            校验位
         """
         code17 = SequenceUtil.sub_lst(s, 0, 17)
         check_sum = 0
@@ -213,13 +305,24 @@ class IDCardUtil(object):
     def get_birthday_from_id(cls, s: str) -> typing.Optional[datetime]:
         """
         从 ID 获取生日 datetime 对象
-        :param s: id身份证号
-        :return: 生日 datetime 对象
+
+        Parameters
+        ----------
+        s : str
+            身份证ID
+
+        Returns
+        -------
+        typing.Optional[datetime]
+            表示生日的 datetime 对象, 如果身份证无效则返回 None
         """
         if not cls.is_valid_id(s):
             return None
         birthday = SequenceUtil.sub_lst(s, 6, 14)
         matched = PatternPool.BIRTHDAY_PATTERN.match(birthday)
+
+        if matched is None:
+            return None
         # 采用正则匹配的方式获取生日信息
         # NOTE 如果上面进行了有效性验证, 那么必然匹配, 所以不需要再次判断
         year = BasicConvertor.to_int(matched.group(1))
@@ -230,8 +333,16 @@ class IDCardUtil(object):
     @classmethod
     def get_card_from_id(cls, id: str) -> IDCard:
         """
-        静态方法获取对象
-        :param id: 身份证 ID
-        :return: IDCard 对象
+        根据身份证 ID 获取身份证对象
+
+        Parameters
+        ----------
+        id : str
+            身份证ID
+
+        Returns
+        -------
+        IDCard
+            身份证对象
         """
         return IDCard(id)
