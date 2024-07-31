700cc4e939c8fdbf8f2e54899aa7b355702bc34c
pancx <p_lord@163.com>
2024-07-31 14:56:11 +0800

    ✨ feat(core): Gender枚举类添加获取方法
    Gender枚举类添加从字符串获取枚举实例的方法，目前支持使用代表性别的字符串获取枚举实例。

--- pythontools/core/constants/people_constant.py
+++ pythontools/core/constants/people_constant.py
@@ -15,10 +15,19 @@ Change Activity:
 """
 
 # here put the import lib
+import typing
 from collections import namedtuple
 from enum import Enum
 
 Person = namedtuple("Person", ["sex", "sex_code"])
+
+
+MALE_SET: typing.FrozenSet[str] = frozenset(["MALE", "male", "男性" "男" "男的"])
+FEMALE_SET: typing.FrozenSet[str] = frozenset([
+    "FEMALE",
+    "female",
+    "女性" "女" "女的",
+])
 
 
 class Gender(Enum):
@@ -26,8 +35,17 @@ class Gender(Enum):
     FEMALE = Person(sex="女性", sex_code=2)
 
     @classmethod
-    def get_sex(cls, code: int) -> "Gender":
+    def get_gender_by_code(cls, code: int) -> "Gender":
         if code % 2 == 1:
             return cls.MALE
         else:
             return cls.FEMALE
+
+    @classmethod
+    def get_gender_by_name(cls, name: str) -> "Gender":
+        if name in MALE_SET:
+            return cls.MALE
+        elif name in FEMALE_SET:
+            return cls.FEMALE
+        else:
+            raise ValueError(f"Invalid name: {name}")
