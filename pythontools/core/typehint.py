6899b9acc19286aa26340b1867b0c87246884efd
pancx <p_lord@163.com>
2024-08-06 09:51:30 +0800

    ✨ feat(core): 添加注解库
    添加注解库，用于统一管理项目中的注解信息

---
+++ pythontools/core/typehint.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+-------------------------------------------------
+@File       :   typehint.py
+@Date       :   2024/08/05
+@Desc       :   None
+@Version    :   1.0
+-------------------------------------------------
+Change Activity:
+@Date       :   2024/08/05
+@Author     :   Plord117
+@Desc       :   None
+-------------------------------------------------
+"""
+
+from os import PathLike
+from typing import TypeAlias, TypeVar
+
+# here put the import lib
+StrOrBytesPath: TypeAlias = str | bytes | PathLike[str] | PathLike[bytes]
+FileDescriptorOrPath: TypeAlias = int | StrOrBytesPath
+T = TypeVar("T")
