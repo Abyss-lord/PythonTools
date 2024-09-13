7008c6be64064dba9807a26ebd2a4d7e4d37625f
plord117 <p_lord@163.com>
2024-09-13 15:37:35 +0800

    🧪 test(tests): 重构布尔工具类测试
     重构布尔工具类测试，使用pytest参数化重构测试

--- tests/boolean_util_test.py
+++ tests/boolean_util_test.py
@@ -36,94 +36,190 @@ class TestBooleanUtil:
     @allure.description("测试布尔值True和False的转换")
     class TestTriggerBoolean:
         @allure.title("测试布尔值转换")
-        def test_negate_with_correct_arguments(cls) -> None:
-            with allure.step("步骤1:测试布尔值取反"):
-                assert not BooleanUtil.negate(True)
-                assert BooleanUtil.negate(False)
-                assert not BooleanUtil.negate(1)  # type: ignore
-                assert BooleanUtil.negate(0)  # type: ignore
+        @pytest.mark.parametrize(
+            "input_value, expected_output",
+            [
+                (True, False),
+                (False, True),
+                (1, False),
+                (0, True),
+            ],
+        )
+        def test_negate_with_correct_arguments(self, input_value, expected_output) -> None:
+            assert BooleanUtil.negate(input_value) == expected_output
 
-            with allure.step("步骤2:测试布尔值转换，输入错误参数"):
-                assert not BooleanUtil.negate(1, raise_exception=False)  # type: ignore
-                with pytest.raises(TypeError):
-                    BooleanUtil.negate(1, raise_exception=True)  # type: ignore
+        @allure.title("测试布尔值转换,输入错误参数")
+        @pytest.mark.parametrize(
+            "input_value",
+            [
+                1,
+            ],
+        )
+        def test_negate_with_wrong_arguments(self, input_value):
+            with pytest.raises(TypeError):
+                BooleanUtil.negate(input_value, raise_exception=True)  # type: ignore
 
     @allure.story("布尔值逻辑运算")
+    @allure.description("测试布尔值逻辑运算")
     class TestBooleanLogic:
         @allure.title("测试与运算")
-        def test_and(cls) -> None:
-            assert BooleanUtil.and_all(True, True)
-            assert not BooleanUtil.and_all(True, False)
-            assert not BooleanUtil.and_all(False, True)
-            assert not BooleanUtil.and_all(False, False)
+        @pytest.mark.parametrize(
+            "input_value1, input_value2, expected_output",
+            [
+                (True, True, True),
+                (True, False, False),
+                (False, True, False),
+                (False, False, False),
+            ],
+        )
+        def test_and(cls, input_value1, input_value2, expected_output) -> None:
+            assert BooleanUtil.and_all(input_value1, input_value2) == expected_output
 
         @allure.title("测试异或运算")
-        def test_xor(self):
-            assert BooleanUtil.xor(True, False)
-            assert not BooleanUtil.xor(True, False, True)
-            assert BooleanUtil.xor(True, False, False)
-            assert not BooleanUtil.xor(True, True)
+        @pytest.mark.parametrize(
+            "input_value1, input_value2, expected_output",
+            [
+                (True, True, False),
+                (True, False, True),
+                (False, True, True),
+                (False, False, False),
+            ],
+        )
+        def test_xor(
+            self,
+            input_value1: bool,
+            input_value2: bool,
+            expected_output: bool,
+        ) -> None:
+            assert BooleanUtil.xor_all(input_value1, input_value2) == expected_output
 
         @allure.title("测试或运算")
-        def test_or(self):
-            assert BooleanUtil.or_all(True, True)
-            assert BooleanUtil.or_all(True, False)
-            assert BooleanUtil.or_all(False, True)
-            assert not BooleanUtil.or_all(False, False)
-            assert not BooleanUtil.or_all(0, 0, 0)
+        @pytest.mark.parametrize(
+            "input_value1, input_value2, expected_output",
+            [
+                (True, True, True),
+                (True, False, True),
+                (False, True, True),
+                (False, False, False),
+            ],
+        )
+        def test_or(self, input_value1, input_value2, expected_output):
+            assert BooleanUtil.or_all(input_value1, input_value2) == expected_output
 
-    @allure.story("布尔值转换")
+    @allure.story("布尔值和其他类型的转换")
+    @allure.description("测试布尔值和其他类型的转换")
     class TestBooleanConversion:
         @allure.title("测试布尔值转换为整数")
-        def test_boolean_to_int_with_correct_arguments(cls) -> None:
-            with allure.step("步骤1:测试布尔值转换为整数,输入正确参数"):
-                assert BooleanUtil.boolean_to_int(True) == 1
-                assert BooleanUtil.boolean_to_int(False) == 0
+        @pytest.mark.parametrize(
+            "input_value, expected_output",
+            [
+                (True, 1),
+                (False, 0),
+                pytest.param(1, 1, marks=pytest.mark.xfail),
+            ],
+        )
+        def test_boolean_to_int_with_correct_arguments(cls, input_value, expected_output) -> None:
+            assert BooleanUtil.boolean_to_int(input_value) == expected_output
 
-            with allure.step("步骤2:测试布尔值转换为整数,输入错误参数"):
-                with pytest.raises(ValueError):
-                    BooleanUtil.boolean_to_int(1, strict_mode=True) == 1
+        @allure.title("测试布尔值转换为 TRUE、FALSE 字符串")
+        @pytest.mark.parametrize(
+            "input_value, expected_output",
+            [
+                (True, "TRUE"),
+                (False, "FALSE"),
+            ],
+        )
+        def test_to_str_true_and_false(
+            cls,
+            input_value,
+            expected_output,
+        ) -> None:
+            assert BooleanUtil.to_str_true_and_false(input_value) == expected_output
 
-        @allure.title("测试布尔值转换为字符串")
-        def test_boolean_to_str(self):
-            with allure.step("步骤1:测试布尔值转换为True和False"):
-                assert BooleanUtil.to_str_true_and_false(True) == "TRUE"
-                assert BooleanUtil.to_str_true_and_false(False) == "FALSE"
+        @allure.title("测试布尔值转换为Yes和No字符串")
+        @pytest.mark.parametrize(
+            "input_value, expected_output",
+            [
+                (True, "YES"),
+                (False, "NO"),
+            ],
+        )
+        def test_to_str_yes_no(
+            self,
+            input_value,
+            expected_output,
+        ):
+            assert BooleanUtil.to_str_yes_no(input_value) == expected_output
 
-            with allure.step("步骤2:测试布尔值转换为Yes和No"):
-                assert BooleanUtil.to_str_yes_no(True) == "YES"
-                assert BooleanUtil.to_str_yes_no(False) == "NO"
+        @allure.title("测试布尔值转换为是和否字符串")
+        @pytest.mark.parametrize(
+            "input_value, expected_output",
+            [
+                (True, "是"),
+                (False, "否"),
+            ],
+        )
+        def test_to_chinese_str(
+            self,
+            input_value,
+            expected_output,
+        ):
+            assert BooleanUtil.to_chinese_str(input_value) == expected_output
 
-            with allure.step("步骤3:测试布尔值转换为是和否"):
-                assert BooleanUtil.to_chinese_str(True) == "是"
-                assert BooleanUtil.to_chinese_str(False) == "否"
+        @allure.title("测试布尔值转换为 ON 和 OFF 字符串")
+        @pytest.mark.parametrize(
+            "input_value, expected_output",
+            [
+                (True, "ON"),
+                (False, "OFF"),
+            ],
+        )
+        def test_to_str_on_and_off(
+            self,
+            input_value,
+            expected_output,
+        ) -> None:
+            assert BooleanUtil.to_str_on_and_off(input_value) == expected_output
 
-            with allure.step("步骤4:测试布尔值转换为ON和OFF"):
-                assert BooleanUtil.to_str_on_and_off(True) == "ON"
-                assert BooleanUtil.to_str_on_and_off(False) == "OFF"
-
-            with allure.step("步骤5:测试布尔值转换为自定义字符串"):
-                assert BooleanUtil.to_string(True, "|", "?") == "|"
-                assert BooleanUtil.to_string(False, "|", "?") == "?"
+        @allure.title("测试布尔值转换为自定义字符串")
+        @pytest.mark.parametrize(
+            "input_value, true_str, false_str, expected_output",
+            [
+                (True, "|", "?", "|"),
+                (False, "|", "?", "?"),
+            ],
+        )
+        def test_to_string(
+            self,
+            input_value,
+            true_str,
+            false_str,
+            expected_output,
+        ) -> None:
+            assert BooleanUtil.to_string(input_value, true_str, false_str) == expected_output
 
         @allure.title("测试字符串转换为布尔值")
-        def test_str_to_boolean(self):
-            with allure.step("步骤1:测试英文字符串转换为布尔值"):
-                assert BooleanUtil.str_to_boolean("True")
-                assert BooleanUtil.str_to_boolean("ok")
-                assert not BooleanUtil.str_to_boolean("false")
-                assert not BooleanUtil.str_to_boolean("no")
-                assert not BooleanUtil.str_to_boolean("×")
-
-            with allure.step("步骤2:测试中文字符串转换为布尔值"):
-                assert BooleanUtil.str_to_boolean("对")
-                assert not BooleanUtil.str_to_boolean("错")
-                assert BooleanUtil.str_to_boolean("是")
-                assert not BooleanUtil.str_to_boolean("否")
-
-            with allure.step("步骤3:测试数字字符串转换为布尔值"):
-                assert not BooleanUtil.str_to_boolean("0")
-                assert BooleanUtil.str_to_boolean("1")
-
-            with allure.step("步骤4:测试空字符串转换为布尔值"):
-                assert not BooleanUtil.str_to_boolean("")
+        @pytest.mark.parametrize(
+            "input_value, expected_output",
+            [
+                ("True", True),
+                ("False", False),
+                ("1", True),
+                ("0", False),
+                ("是", True),
+                ("否", False),
+                ("ON", True),
+                ("OFF", False),
+                ("对", True),
+                ("错", False),
+                ("ok", True),
+                ("no", False),
+                ("", False),
+            ],
+        )
+        def test_str_to_boolean(
+            self,
+            input_value,
+            expected_output,
+        ) -> None:
+            assert BooleanUtil.str_to_boolean(input_value) == expected_output
